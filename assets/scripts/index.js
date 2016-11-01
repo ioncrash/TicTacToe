5978a1519aa1ba237c7145826cb5e192b93a4cdf
ioncrash <ioncrash@gmail.com>
2016-10-31 23:03:59 -0400

    copy assets/scripts/index.js
    code still works

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,8 +1,41 @@
 'use strict';
 
-const authEvents = require('./auth/events.js');
+// user require with a reference to bundle the file and use it in this file
+// var example = require('./example');
 
-// On document ready
-$(() => {
-  authEvents.addHandlers();
+// use require without a reference to ensure a file is bundled
+require('./example');
+
+$(()=>{
+  $('.sign-up-form').on('submit', function(e){
+    e.preventDefault();
+    let email = $('#sign-up-email').val();
+    let password = $('#sign-up-password').val();
+    let confirmPassword = $('#confirm-password').val();
+    debugger;
+    $('#sign-up-modal').modal('hide');
+  });
+
+
+  $('.sign-in-form').on('submit', function(e){
+    e.preventDefault();
+    let email = $('#sign-in-email').val();
+    let password = $('#sign-in-password').val();
+    debugger;
+
+    $('#sign-in-modal').modal('hide');
+  });
+
+  $('.change-password-form').on('submit', function(e){
+    e.preventDefault();
+    let oldPassword = $('#old-password').val();
+    let newPassword = $('#new-password').val();
+    debugger;
+
+    $('#change-password-modal').modal('hide');
+  });
+
+  $('#sign-out-button').on('click', function(e){
+    alert('you have signed out!');
+  });
 });
