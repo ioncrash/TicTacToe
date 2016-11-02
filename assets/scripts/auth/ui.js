0a8c7fc2efce4caba899aa8583a491d2a13adce6
ioncrash <ioncrash@gmail.com>
2016-11-01 21:53:21 -0400

    add player bars w/ jquery calls on signin
    now add signouts!

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -11,10 +11,12 @@ const signInSuccess = (data) => {
     store.player_x = {
       user: data.user
     };
+    $('#player-x-bar').text('Player X: ' + store.player_x.user.email);
   } else if (!store.player_o) {
     store.player_o = {
       user: data.user
     };
+    $('#player-o-bar').text('Player O: ' + store.player_o.user.email);
   }
     success(data);
 };
@@ -23,6 +25,8 @@ const failure = (error) => {
   $('#messages').text('fail');
   console.error(error);
 };
+
+
 
 module.exports = {
   failure,
