a1c562b3bf5d3ab96ff1cf4226905ee7b6d7a3d2
ioncrash <ioncrash@gmail.com>
2016-11-02 00:26:21 -0400

    init game files
    but need to return to api to change game start logic...

---
+++ assets/scripts/game/model.js
@@ -0,0 +1,15 @@
+'use strict';
+
+ let sample = {
+      "id": 2,
+      "cells": ["","","","","","","","",""],
+      "over": false,
+      "player_x": {
+        "id": 3,
+        "email": "dna@dna.com"
+      },
+      "player_o": {
+        "id": 1,
+        "email": "and@and.com"
+      }
+    };
