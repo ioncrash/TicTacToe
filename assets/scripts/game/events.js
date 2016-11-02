029d7c1e0d208a09594bf703ac6beb7b6a7216e9
ioncrash <ioncrash@gmail.com>
2016-11-02 09:59:24 -0400

    add all box click events
    they work

--- assets/scripts/game/events.js
+++ assets/scripts/game/events.js
@@ -5,12 +5,60 @@ const ui = require('./ui.js');
 
 const boxZero = function(e) {
   e.preventDefault();
-  debugger;
+
+};
+
+const boxOne = function(e) {
+  e.preventDefault();
+
+};
+
+const boxTwo = function(e) {
+  e.preventDefault();
+
+};
+
+const boxThree = function(e) {
+  e.preventDefault();
+
+};
+
+const boxFour = function(e) {
+  e.preventDefault();
+
+};
+
+const boxFive = function(e) {
+  e.preventDefault();
+
+};
+
+const boxSix = function(e) {
+  e.preventDefault();
+
+};
+
+const boxSeven = function(e) {
+  e.preventDefault();
+
+};
+
+const boxEight = function(e) {
+  e.preventDefault();
+
 };
 
 const addBoxHandlers = function() {
+  $('.box').addClass('active-box');
   $('#box-0').on('click', boxZero);
-  $('#box-0').addClass('active-box');
+  $('#box-1').on('click', boxOne);
+  $('#box-2').on('click', boxTwo);
+  $('#box-3').on('click', boxThree);
+  $('#box-4').on('click', boxFour);
+  $('#box-5').on('click', boxFive);
+  $('#box-6').on('click', boxSix);
+  $('#box-7').on('click', boxSeven);
+  $('#box-8').on('click', boxEight);
 };
 
 const startGame = function() {
