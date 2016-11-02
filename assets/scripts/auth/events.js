80fdbbc3f50eb557e356624f4ef6ec0cb5af31aa
ioncrash <ioncrash@gmail.com>
2016-11-01 23:28:14 -0400

    add ifs to check signin before calling api.js
    seems to work!

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -7,7 +7,6 @@ const store = require('../store.js');
 
 const onSignUp = function(e){
   e.preventDefault();
-
   let data = getFormFields(this);
   api.signUp(data).then(ui.success).catch(ui.failure);
   $('#sign-up-modal').modal('hide');
@@ -23,26 +22,32 @@ const onSignIn = function(e){
 const onXChangePassword = function(e){
   let data = getFormFields(this);
   e.preventDefault();
-  api.changeXPassword(data)
-    .then(ui.success)
-    .catch(ui.failure);
+  if (store.player_x) {
+    api.changeXPassword(data)
+      .then(ui.success)
+      .catch(ui.failure);
+    }
   $('#change-x-password-modal').modal('hide');
 };
 
 const onOChangePassword = function(e){
   let data = getFormFields(this);
   e.preventDefault();
-  api.changeOPassword(data)
-    .then(ui.success)
-    .catch(ui.failure);
+  if (store.player_o) {
+    api.changeOPassword(data)
+      .then(ui.success)
+      .catch(ui.failure);
+    }
   $('#change-o-password-modal').modal('hide');
 };
 
 const onSignOutX = function(e){
   e.preventDefault();
-  api.signOutX()
-    .then(ui.success)
-    .catch(ui.failure);
+  if (store.player_x) {
+    api.signOutX()
+      .then(ui.success)
+      .catch(ui.failure);
+    }
   $('#player-x-bar').text('Player X: ');
   store.player_x = null;
   $("#start-game-button").hide();
@@ -50,9 +55,11 @@ const onSignOutX = function(e){
 
 const onSignOutO = function(e){
   e.preventDefault();
-  api.signOutO()
-    .then(ui.success)
-    .catch(ui.failure);
+  if (store.player_o) {
+    api.signOutO()
+      .then(ui.success)
+      .catch(ui.failure);
+    }
   $('#player-o-bar').text('Player O: ');
   store.player_o = null;
   $("#start-game-button").hide();
