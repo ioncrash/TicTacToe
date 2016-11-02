caca2436c68bbdcfe1b764af31ce9978fba73218
ioncrash <ioncrash@gmail.com>
2016-11-01 22:16:41 -0400

    begin checkReady
    must clear local store on sign out!

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -6,17 +6,25 @@ const success = (data) => {
   console.log(data);
 };
 
+const checkReady = function() {
+  if (store.player_x && store.player_o) {
+    debugger;
+  }
+};
+
 const signInSuccess = (data) => {
   if (!store.player_x) {
     store.player_x = {
       user: data.user
     };
     $('#player-x-bar').text('Player X: ' + store.player_x.user.email);
+    checkReady();
   } else if (!store.player_o) {
     store.player_o = {
       user: data.user
     };
     $('#player-o-bar').text('Player O: ' + store.player_o.user.email);
+    checkReady();
   }
     success(data);
 };
